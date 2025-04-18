e62293a6ceae70dbbe1b91170c26cb4f54bfa44e
melek saidani <mbcmelek3@gmail.com>
2025-04-18 11:30:59 +0100

    Update omni_j2y18lte.mk

--- omni_j2y18lte.mk
+++ omni_j2y18lte.mk
@@ -4,14 +4,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# Embedded
 
-# Inherit some common Omni stuff.
+$(call inherit-product, build/target/product/embedded.mk)
+
+# Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-# Inherit from j2y18lte device
-$(call inherit-product, device/samsung/j2y18lte/device.mk)
 
 PRODUCT_DEVICE := j2y18lte
 PRODUCT_NAME := omni_j2y18lte
