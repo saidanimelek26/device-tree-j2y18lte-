19052523e84dd7d066f523fb4b5772dceb9a420a
melek saidani <mbcmelek3@gmail.com>
2025-04-18 12:57:28 +0100

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -3,9 +3,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),j2y18lte)
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
-endif
-
+include $(call all-makefiles-under,$(LOCAL_PATH))
